8d4931a58b469cacc986219bcf0e469bed14328d
MasterBoy1616 <130065747+MasterBoy1616@users.noreply.github.com>
2025-04-16 21:20:27 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,5 @@
-import React from "react";
 import FiyatSorgulama from "./FiyatSorgulama";
+
 export default function App() {
   return <FiyatSorgulama />;
 }
-
-
-function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Çalışkanel Servis – Fiyat Sorgulama Sistemi</h1>
-      <p>Marka/model/tip ve parça detayları yakında burada olacak.</p>
-    </div>
-  );
-}
-
-export default App;
